8d48ddc012012a141ce946202c95fe39a7d437fa
cortex-145f <cybercortex29@gmail.com>
2026-06-12 14:50:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://repository-images.githubusercontent.com/78524793/17d15480-1b36-11ea-8472-dc52b86f3e2b",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Cybercortex1 Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
